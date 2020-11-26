015b8c11070774153f7e1087a77f4954a6351493
LastSicarius <lastsicarius@protonmail.com>
2020-11-26 19:17:09 +0100

    Initial commit

---
+++ src/main/java/com/fpl/statistics/fpls/FplsApplication.java
@@ -0,0 +1,13 @@
+package com.fpl.statistics.fpls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FplsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpl/statistics/fpls/FplsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpl.statistics.fpls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
